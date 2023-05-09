7361e27eea870659be80bec01bac647c292bba4a
Rifqi Padi Siliwangi <79899198+Rifqips@users.noreply.github.com>
2023-05-09 17:02:00 +0700

    switch case
    add birth case

---
+++ Tugas-2B-konditional/switch_case.dart
@@ -0,0 +1,14 @@
+void main(List<String> args) {
+  var tanggal = 1;
+  var bulan = "mei";
+  var tahun = 1945;
+  var addValue = 1;
+  var result = '$tanggal $bulan $tahun';
+
+  switch (addValue) {
+    case 1:
+      print(result);
+      break;
+    default:
+  }
+}
